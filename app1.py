b160ef5e11475589b6a6165eee9545e5b350299f
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2023-11-21 07:02:26 -0600

    Working

--- app1.py
+++ app1.py
@@ -1,52 +1,43 @@
 from flask import Flask, render_template, request, jsonify
-from google.cloud import storage
 from PyPDF2 import PdfReader
 import openai
 
 app = Flask(__name__)
-summaries = {}
+summaries = {}  # Dictionary to store summaries
 
-# Initialize Google Cloud Storage client
-storage_client = storage.Client()
-bucket_name = 'your_bucket_name'  # Replace with your GCS bucket name
-
-openai.api_key = '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'
-MAX_TOKENS = 4000
-
+# Set your OpenAI API key
+openai.api_key = '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'
 @app.route('/')
 def index():
     return render_template('index1.html')
 
-@app.route('/api/upload_and_summarize', methods=['POST'])
-def upload_and_summarize():
-    if 'file' in request.files:
+@app.route('/api/summarize_text', methods=['POST'])
+def summarize_text():
+    if request.method == 'POST':
+        data = request.get_json()
+        text = data.get('text')
+        if not text:
+            return jsonify({'error': 'No text found'})
+        summary = generate_summary(text)
+        summary_id = len(summaries) + 1
+        summaries[summary_id] = summary
+        return jsonify({'summary_id': summary_id})
+
+@app.route('/api/summarize_file', methods=['POST'])
+def summarize_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file found'})
+        
         pdf_file = request.files['file']
-        if pdf_file.filename != '':
-            # Upload file to Google Cloud Storage
-            blob = storage_client.bucket(bucket_name).blob(pdf_file.filename)
-            blob.upload_from_file(pdf_file)
-
-            # Extract text from the uploaded PDF file
-            content = extract_text_from_pdf(pdf_file)
-
-            # Generate summary from the extracted text
-            summary = generate_summary(content)
-            summary_id = len(summaries) + 1
-            summaries[summary_id] = summary
-
-            return jsonify({'summary_id': summary_id})
-    
-    elif 'text' in request.json:
-        text = request.json['text']
-        if text:
-            # Generate summary from the input text
-            summary = generate_summary(text)
-            summary_id = len(summaries) + 1
-            summaries[summary_id] = summary
-
-            return jsonify({'summary_id': summary_id})
-    
-    return jsonify({'error': 'Invalid input'})
+        content = extract_text_from_pdf(pdf_file)
+        if not content:
+            return jsonify({'error': 'Failed to extract text from the PDF'})
+        
+        summary = generate_summary(content)
+        summary_id = len(summaries) + 1
+        summaries[summary_id] = summary
+        return jsonify({'summary_id': summary_id})
 
 @app.route('/summary/<int:summary_id>')
 def show_summary(summary_id):
@@ -65,25 +56,20 @@ def extract_text_from_pdf(pdf_file):
     return text
 
 def generate_summary(content):
-    chunks = [content[i:i + MAX_TOKENS] for i in range(0, len(content), MAX_TOKENS)]
-    summaries = []
-    for chunk in chunks:
-        prompt = f"Summarize the following text:\n\n{chunk}"
+    prompt = f"Summarize the following text:\n\n{content}"
 
-        response = openai.Completion.create(
-            engine="text-davinci-002",
-            prompt=prompt,
-            max_tokens=150,
-            temperature=0.7,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0,
-        )
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        max_tokens=150,
+        temperature=0.7,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.0,
+    )
 
-        summary = response.choices[0].text.strip()
-        summaries.append(summary)
-
-    return ' '.join(summaries)
+    summary = response.choices[0].text.strip()
+    return summary
 
 if __name__ == '__main__':
     app.run(debug=True)
