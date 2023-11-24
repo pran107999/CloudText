c2b6c622422359fc5323b853dece11f1af568d5a
Vineet Kurapati <89411810+VineetKurapati@users.noreply.github.com>
2023-11-23 19:53:09 -0600

    23rdNov

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,46 +1,45 @@
 from flask import Flask, render_template, request, jsonify
-from google.cloud import storage
 from PyPDF2 import PdfReader
 import openai
 
 app = Flask(__name__)
-summaries = {}
+summaries = {} 
 
 
-openai.api_key = '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'
-MAX_TOKENS = 4000
+openai.api_key = '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'
+MAX_TOKENS = 4000  
 
 @app.route('/')
 def index():
     return render_template('index1.html')
 
-@app.route('/api/upload_and_summarize', methods=['POST'])
-def upload_and_summarize():
-    if 'file' in request.files:
+@app.route('/api/summarize_text', methods=['POST'])
+def summarize_text():
+    if request.method == 'POST':
+        data = request.get_json()
+        text = data.get('text')
+        if not text:
+            return jsonify({'error': 'No text found'})
+        summary = generate_summary(text)
+        summary_id = len(summaries) + 1
+        summaries[summary_id] = summary
+        return jsonify({'summary_id': summary_id})
+
+@app.route('/api/summarize_file', methods=['POST'])
+def summarize_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({'error': 'No file found'})
+        
         pdf_file = request.files['file']
-        if pdf_file.filename != '':
-
-            # Extract text from the uploaded PDF file
-            content = extract_text_from_pdf(pdf_file)
-
-            # Generate summary from the extracted text
-            summary = generate_summary(content)
-            summary_id = len(summaries) + 1
-            summaries[summary_id] = summary
-
-            return jsonify({'summary_id': summary_id})
-    
-    elif 'text' in request.json:
-        text = request.json['text']
-        if text:
-            # Generate summary from the input text
-            summary = generate_summary(text)
-            summary_id = len(summaries) + 1
-            summaries[summary_id] = summary
-
-            return jsonify({'summary_id': summary_id})
-    
-    return jsonify({'error': 'Invalid input'})
+        content = extract_text_from_pdf(pdf_file)
+        if not content:
+            return jsonify({'error': 'Failed to extract text from the PDF'})
+        
+        summary = generate_summary(content)
+        summary_id = len(summaries) + 1
+        summaries[summary_id] = summary
+        return jsonify({'summary_id': summary_id})
 
 @app.route('/summary/<int:summary_id>')
 def show_summary(summary_id):
@@ -59,6 +58,7 @@ def extract_text_from_pdf(pdf_file):
     return text
 
 def generate_summary(content):
+    # Split content into chunks of maximum token length
     chunks = [content[i:i + MAX_TOKENS] for i in range(0, len(content), MAX_TOKENS)]
     summaries = []
     for chunk in chunks:
@@ -68,11 +68,12 @@ def generate_summary(content):
             engine="text-davinci-002",
             prompt=prompt,
             max_tokens=150,
-            temperature=0.9,
+            temperature=0.7,
             top_p=1.0,
             frequency_penalty=0.0,
             presence_penalty=0.0,
         )
+
         summary = response.choices[0].text.strip()
         summaries.append(summary)
 
